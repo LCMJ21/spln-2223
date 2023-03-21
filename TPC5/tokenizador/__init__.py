403adf2488aeb7118772ae680f17836ab0b9869a
LCMJ21 <jorge.lima.ne@gmail.com>
2023-03-21 16:58:05 +0000

    with translations

--- TPC5/tokenizador/__init__.py
+++ TPC5/tokenizador/__init__.py
@@ -11,7 +11,7 @@ import re
 
 def load_abrevs():
     abrevs_dict = {}
-    file = open("TPC4/tokenizador/conf/abervs.txt", "r")
+    file = open("TPC5/tokenizador/conf/abervs.txt", "r")
     ln_list = file.read().split("#")[1:]
     for ln in ln_list:
         lang, *abrevs = ln.split("\n")
@@ -19,7 +19,20 @@ def load_abrevs():
             abrevs_dict[lang] = abrevs if abrevs[-1] else abrevs[:-1]
     return abrevs_dict
 
-print(load_abrevs())
+def load_trans():
+    trans_dict = {}
+    file = open("TPC5/tokenizador/conf/trans.txt", "r")
+    pal_list = file.read().split("#")[1:]
+    for pal in pal_list:
+        en_pal, *trans_pals = pal.split("\n")
+        trans_dict[en_pal] = {}
+        for trans in trans_pals:
+            lang_id, *lang_trans = trans.split(":")
+            if len(lang_trans) == 1:
+                trans_dict[en_pal][lang_id] = lang_trans[0]
+    return trans_dict
+
+print(load_trans())
 
 def tokenizador():
     text = ""
